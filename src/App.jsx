0a5d32fc938c2a25393bce6b9b6ce625d5a3d739
unknown <muzibmuzakar33@gmail.com>
2023-01-11 09:40:05 +0700

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
+import Header from './components/Header';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
 
   return (
-    <div className="App bg-red-700">
-      <h1 className="text-3xl font-bold underline bg-red-600">
-      Hello world!
-      </h1>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,102 @@
+import Logo from './assets/img/logo/logo-bpl.png';
+// logo club
+import Adios from './assets/img/logo/adiosfc.png';
+import Alkanara from './assets/img/logo/alkanara.png';
+import Andir from './assets/img/logo/andirunited.png';
+import Bagundal from './assets/img/logo/bagundal.png';
+import Mengbal from './assets/img/logo/bandungmengbal.png';
+import Pride from './assets/img/logo/bandungpride.png';
+import Banzai from './assets/img/logo/banzai.png';
+import Baraya from './assets/img/logo/barayajabar.png';
+import Besitua from './assets/img/logo/besitua.png';
+import Bonesc from './assets/img/logo/bonesc.png';
+import Buciper from './assets/img/logo/buciper.png';
+import Expose from './assets/img/logo/expose.png';
+import Noname from './assets/img/logo/noname.png';
+import Ortufc from './assets/img/logo/ortufc.png';
+import Popthree from './assets/img/logo/popthree.png';
+import Prima from './assets/img/logo/primafc.png';
+import World from './assets/img/logo/worldyoungunited.png';
+import Xtujuhbelas from './assets/img/logo/x17.png';
+
+export const navigation = [
+    {
+      name: 'home',
+      href: '/',
+    },
+    {
+      name: 'news',
+      href: 'news',
+    },
+    {
+      name: 'fixtures',
+      href: 'fixtures',
+    },
+    {
+      name: 'results',
+      href: 'results',
+    },
+    {
+      name: 'tables',
+      href: 'tables',
+    },
+    {
+      name: 'statistics',
+      href: 'statistics',
+    },
+    {
+      name: 'clubs',
+      href: 'clubs',
+    },
+    {
+      name: 'store',
+      href: 'store',
+    },
+];
+
+export const matches = [
+    {
+        game: 1,
+        home: 'Adios FC',
+        logoHome: Adios ,
+        away: 'Alkanar',
+        logoAway: Alkanara,
+        vanue: 'GBLA',
+        date: '1 Januari 2022',
+        time: '20:00 WIB',
+        league: Logo
+    },
+    {
+        game: 1,
+        home: 'Andir United',
+        logoHome: Andir,
+        away: 'Bagundal',
+        logoAway: Bagundal,
+        vanue: 'GBLA',
+        date: '1 Januari 2022',
+        time: '20:00 WIB',
+        league: Logo
+    },
+    {
+        game: 1,
+        home: 'Bandung Mengbal',
+        logoHome: Mengbal,
+        away: 'Bandung Pride',
+        logoAway: Pride,
+        vanue: 'Jalak Harupan',
+        date: '1 Januari 2022',
+        time: '20:00 WIB',
+        league: Logo
+    },
+    {
+        game: 2,
+        home: 'Banzai',
+        logoHome: Banzai,
+        away: 'Baraya Jabar',
+        logoAway: Baraya,
+        vanue: 'Jalak Harupat',
+        date: '1 Januari 2022',
+        time: '20:00 WIB',
+        league: Logo
+    },
+];
